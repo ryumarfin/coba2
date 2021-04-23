2926a7c07623387089e25d6ae61762f29ec81484
susisyahrir <susisyahrir@gmail.com>
2021-04-23 17:34:09 +0700

    Spinner ok

--- app/src/main/java/com/example/coba2/SecondActivity.kt
+++ app/src/main/java/com/example/coba2/SecondActivity.kt
@@ -3,6 +3,11 @@ package com.example.coba2
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_second.*
 import java.util.*
 
@@ -33,7 +38,34 @@ class SecondActivity : AppCompatActivity() {
                             // Display Selected date in TextView
                             tanggal.setText("$dayOfMonth / ${monthOfYear+1} / $year")
                 }, year, month, day)
+            datePickerDialog.datePicker.minDate = Calendar.getInstance().timeInMillis
             datePickerDialog.show()
+        }
+
+        val hari = resources.getStringArray(R.array.hari)
+
+        val spinner = findViewById<Spinner>(R.id.myspinner)
+        if(spinner!=null){
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_dropdown_item, hari)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                    AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                    Toast.makeText(this@SecondActivity, hari[position], Toast.LENGTH_SHORT).show()
+                    total.text = (m?.Harga * (position+1)).toString()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+
+        btn_Rent.setOnClickListener {
+
         }
     }
 }
